1c3e46819f1d2f148eeee4edbc207561fa5d1ff9
navneetdavang <navneetdavang@gmail.com>
2024-04-12 17:59:48 +0530

    Initial Commit: Project Configuration

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config';
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.get('/', (_, res) =>
+	res.status(200).send('Hello, World from notezy-server'),
+);
+
+app.listen(Number(PORT), () => {
+	console.info(`notezy-server is listening on PORT:${PORT}`);
+});
