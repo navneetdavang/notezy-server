1378d56527b44b43fdd667480e604dbbeba7cb5a
navneetdavang <navneetdavang@gmail.com>
2024-04-14 21:07:09 +0530

    Added support of logging using winston to log on console and transport to log files

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -1,33 +1,45 @@
 import { RequestHandler } from 'express';
+import { isValidObjectId } from 'mongoose';
 
 import NoteModel, { Note } from '../models/note';
-import { isValidObjectId } from 'mongoose';
+import { logger } from '../utils/logger';
 
 export const getNotes: RequestHandler = async (_, res, next) => {
 	try {
+		logger.debug('Executing getNotes controller');
+
 		const notes = await NoteModel.find();
 		return res.status(200).json({
 			notes,
 		});
 	} catch (error) {
+		logger.error(
+			`getNotes controller, Error: ${(error as Error).message}`,
+		);
 		next(error);
 	}
 };
 
 export const createNote: RequestHandler = async (req, res, next) => {
 	try {
+		logger.debug('Executing createNote controller');
 		const note: Note = req.body;
 		const response = await NoteModel.create(note);
 		return res.status(201).json({
 			id: response.id,
 		});
 	} catch (error) {
+		logger.error(
+			`createNote controller, Error: ${(error as Error).message}`,
+		);
 		next(error);
 	}
 };
 
 export const getNoteById: RequestHandler = async (req, res, next) => {
 	try {
+		logger.debug('Executing getNoteById controller');
+
 		const { noteId } = req.params;
 
 		if (!isValidObjectId(noteId))
@@ -46,12 +58,17 @@ export const getNoteById: RequestHandler = async (req, res, next) => {
 
 		return res.status(200).json(response);
 	} catch (error) {
+		logger.error(
+			`getNoteById controller, Error: ${(error as Error).message}`,
+		);
 		next(error);
 	}
 };
 
 export const deleteNoteById: RequestHandler = async (req, res, next) => {
 	try {
+		logger.debug('Executing deleteNoteById controller');
+
 		const { noteId } = req.params;
 
 		if (!isValidObjectId(noteId))
@@ -68,6 +85,9 @@ export const deleteNoteById: RequestHandler = async (req, res, next) => {
 
 		return res.status(200).json();
 	} catch (error) {
+		logger.error(
+			`deleteNoteById controller, Error: ${(error as Error).message}`,
+		);
 		next(error);
 	}
 };
--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,3 +1,17 @@
+export enum AppEnvirnoment {
+	DEVELOPMENT = 'development',
+	PRODUCTION = 'production',
+	TEST = 'test',
+}
+
+export enum LogLevel {
+	ERROR = 'error',
+	WARN = 'warn',
+	INFO = 'info',
+	HTTP = 'http',
+	DEBUG = 'debug',
+}
+
 export enum Collections {
 	NOTES = 'Note',
 }
@@ -11,4 +25,22 @@ export const BaseEndpoint = '/api';
 
 export const Endpoints = {
 	NOTES: `${BaseEndpoint}/notes`,
+};
+
+// Security levels of logging
+export const loggerLevels = {
+	error: 0,
+	warn: 1,
+	info: 2,
+	http: 3,
+	debug: 4,
+};
+
+// Logging levels color configurations
+export const loggerLevelColors = {
+	error: 'red',
+	warn: 'yellow',
+	info: 'green',
+	http: 'magenta',
+	debug: 'white',
 };
