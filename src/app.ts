3f5b989bb042c3e7f22c0ff46e9bd754d5e93899
navneetdavang <74298809+navneetdavang@users.noreply.github.com>
2024-10-26 22:37:54 +0530

    Configured CORS and Helmet Middleware (#16)
    * feat: configured helmet & cors middleware
    
    * feat: replaced origin string with regex

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,44 @@
+import cors from 'cors';
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 
 import errorsMiddleware from './middlewares/errors.middleware';
 import morganMiddleware from './middlewares/morgan.middleware';
 import notesRouter from './routes/notes.route';
-import { Endpoints, ResponseMessage } from './utils/contants';
+import {
+	AppEnvirnoment,
+	Endpoints,
+	ResponseMessage,
+} from './utils/contants';
 import { logger } from './utils/logger';
+import { Env } from './utils/validateEnv';
+
+const { APP_ENV } = Env;
 
 const app = express();
 
+// Configuring the Cors & Helmet
+app.use(
+	helmet({
+		xPoweredBy: false,
+	}),
+);
+app.use(
+	cors({
+		origin:
+			APP_ENV === AppEnvirnoment.DEVELOPMENT
+				? [/^http:\/\/localhost:\d+$/]
+				: '*',
+		credentials: true,
+		optionsSuccessStatus: 200,
+		preflightContinue: true,
+	}),
+);
+
+// Configuring logger middleware
 app.use(morganMiddleware);
+
+// Configuring express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
